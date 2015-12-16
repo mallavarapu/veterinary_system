565cc50a6ec92b6e2fb8553fcf51180fad1286cb
Rama Siva Mallavarapu <ramasiva.mallavarapu@gmail.com>
2015-12-16 08:59:24 -0500

    Updates to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   resources :doctors, only: [:index, :show]
+  resources :customers, only: [:index, :show, :new, :create] do
+    resources :pets, only: [:show, :new, :create] do
+      resources :appointments, only: [:show, :new, :create]
+    end
+  end
 end
